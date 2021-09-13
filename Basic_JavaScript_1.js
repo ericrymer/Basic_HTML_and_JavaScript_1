5b48480d731ade1c8b4de711f1fcafd6a1242719
ericrymer <eric.rymer@gmail.com>
2021-09-13 08:54:04 -0700

    Initial commit

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+	var String = "General Kenobi. You are a bold one.";
+	var result = String.fontcolor("green");
+	document.getElementById("Hello There").innerHTML = 
+result;} 
+	
